bcee416214a88d9c87b1026c9f339f400d0ce7d0
sujingurung99@gmail.com <sujingurung99@gmail.com>
2023-07-12 05:56:15 +0800

    Added session caching and basic QOL features

--- SnPy-500/SnPy_500.py
+++ SnPy-500/SnPy_500.py
@@ -1,3 +1,4 @@
+from time import sleep
 import numpy as np
 import pandas as pd
 import requests
@@ -5,38 +6,56 @@ import xlsxwriter
 import math
 import json
 import yfinance as yf
+import requests_cache
+from requests import Session
+from requests_cache import CacheMixin, SQLiteCache
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
+from pyrate_limiter import Duration, RequestRate, Limiter
 
+counter = 0
 
 #List of S&P500 constituents
-stocks = pd.read_csv('sp_500_stocks.csv')
+stocks_list = pd.read_csv('sp_500_stocks.csv')
+stocks = " "
+stocks = stocks.join(stocks_list['Ticker'].tolist())
+
+#Initializes a session to cache scrapped stock information in case of errors
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    pass
+
+#Session class, from yfinance documentation
+session = CachedLimiterSession(
+    limiter=Limiter(RequestRate(2, Duration.SECOND*5)),  # max 2 requests per 5 seconds
+    bucket_class=MemoryQueueBucket,
+    backend=SQLiteCache("yfinance.cache"),
+)
+
+tickers = yf.Tickers(stocks, session = session)
+
 
 #Building Pandas DataFrame
 my_columns = ['Ticker', 'Stock Price', 'Market Capitlization', ' Number of Shares to Buy']
 final_dataframe = pd.DataFrame(columns = my_columns)
 
 #Looping through all constituents
-for stock in stocks['Ticker']:
+for stock in stocks_list['Ticker']:
+    print(counter)
 
-    #Find the market capitalization and price of the selected company
-    current_price = yf.Ticker(stock).info['currentPrice']
-    market_cap = yf.Ticker(stock).info['marketCap']
+    current_ticker = tickers.tickers[str.upper(stock)]
 
-    final_dataframe = final_dataframe._append(pd.Series([stock,
+    #Find the market capitalization and price of the selected constituent stock
+    try:
+        current_price = current_ticker.info['currentPrice']
+        market_cap = current_ticker.info['marketCap']
+    except KeyError:
+        print("Error")
+
+    #Append to Pandas Dataframe
+    final_dataframe = final_dataframe._append(pd.Series([str.upper(stock),
                                                         current_price,
                                                         market_cap,
-                                                       'N/A'],
+                                                        'N/A'],
                                                         index = my_columns), ignore_index = True)
+    counter += 1
 
 print(final_dataframe)
-
-
-
-##Testing stuff
-#msft = yf.Ticker("MSFT")
-#print("Microsoft Current Market Price : ", msft.info['currentPrice'])
-
-#def search_key_by_keyword(data: dict, keyword: str) -> list[str]:
-#    return list(filter(lambda x: keyword in x.lower(), data.keys()))
-
-#valid_market_keys = search_key_by_keyword(msft.info, "cap")
-#print(valid_market_keys)
