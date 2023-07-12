e985f215c4aa7c100d199616227ad989316d3391
sujingurung99@gmail.com <sujingurung99@gmail.com>
2023-07-12 21:53:59 +0800

    Added functionality to input portfolio size and return number of shares to buy

--- SnPy-500/SnPy_500.py
+++ SnPy-500/SnPy_500.py
@@ -1,4 +1,3 @@
-from time import sleep
 import numpy as np
 import pandas as pd
 import requests
@@ -7,12 +6,12 @@ import math
 import json
 import yfinance as yf
 import requests_cache
+from tqdm import tqdm
+from time import sleep
 from requests import Session
 from requests_cache import CacheMixin, SQLiteCache
 from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 from pyrate_limiter import Duration, RequestRate, Limiter
-
-counter = 0
 
 #List of S&P500 constituents
 stocks_list = pd.read_csv('sp_500_stocks.csv')
@@ -34,21 +33,22 @@ tickers = yf.Tickers(stocks, session = session)
 
 
 #Building Pandas DataFrame
-my_columns = ['Ticker', 'Stock Price', 'Market Capitlization', ' Number of Shares to Buy']
+my_columns = ['Ticker', 'Stock Price', 'Market Capitlization', 'Number of Shares to Buy']
 final_dataframe = pd.DataFrame(columns = my_columns)
 
 #Looping through all constituents
-for stock in stocks_list['Ticker']:
-    print(counter)
-
+for stock in tqdm(stocks_list['Ticker']):
     current_ticker = tickers.tickers[str.upper(stock)]
 
     #Find the market capitalization and price of the selected constituent stock
     try:
         current_price = current_ticker.info['currentPrice']
+    except KeyError:
+        current_price = 'Error'
+    try:
         market_cap = current_ticker.info['marketCap']
     except KeyError:
-        print("Error")
+        market_cap = 'Error'
 
     #Append to Pandas Dataframe
     final_dataframe = final_dataframe._append(pd.Series([str.upper(stock),
@@ -56,6 +56,26 @@ for stock in stocks_list['Ticker']:
                                                         market_cap,
                                                         'N/A'],
                                                         index = my_columns), ignore_index = True)
-    counter += 1
+
+#Calculate the number of shares to Buy
+portfolio_size = input('Enter the value of your portfolio:')
+
+try:
+    val = float(portfolio_size)
+except ValueError:
+    print('That is not an integer value. \nPlease try again:')
+    portfolio_size = input('Enter the value of your portfolio:')
+    val = float(portfolio_size)
+
+#Equal weight position size, divides portfolio size by number of stocks
+position_size = val/len(final_dataframe.index)
+
+#Iterate over pandas dataframe to find number of shares to buy
+for i in range(0, len(final_dataframe.index)):
+    #Round down for margin of error in case of fractional stocks
+    try:
+        final_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[i, 'Stock Price'])
+    except TypeError:
+        final_dataframe.loc[i, 'Number of Shares to Buy'] = 'Error'
 
 print(final_dataframe)
